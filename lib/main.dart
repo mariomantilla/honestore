adfc3d03275e41a355e7e7a3d2a667211e9cff9c
Mario Mantilla <mariomantillasanchez@gmail.com>
2022-06-29 21:28:21 +0200

    Added first backend and simple list of shops

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 const Map<int, Color> color = {
   50: Color.fromRGBO(251, 113, 104, .1),
@@ -17,7 +18,19 @@ class CustomColors {
   static const primary = MaterialColor(0x00fb7168, color);
 }
 
-void main() {
+const supabaseUrl = "https://tbhtpkmrwtznqzsjlfmo.supabase.co";
+const supabaseKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRiaHRwa21yd3R6bnF6c2psZm1vIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTY1MjU1MTUsImV4cCI6MTk3MjEwMTUxNX0.1udAz1lLiqT4bmRF-SZlSNOgng2pCZqwkMAXHb07Ch4';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+      url: supabaseUrl,
+      anonKey: supabaseKey,
+      authCallbackUrlHostname: 'login-callback', // optional
+      debug: true // optional
+      );
   runApp(const MyApp());
 }
 
@@ -28,19 +41,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Honestore',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: CustomColors.primary,
-      ),
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: CustomColors.primary,
+          iconTheme: IconThemeData(color: CustomColors.primary[900])),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -67,6 +80,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  // List<String> shops = List<String>.generate(10000, (i) => 'Shop $i');
+
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
@@ -80,53 +95,31 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    Future<PostgrestResponse> data =
+        Supabase.instance.client.from('shops').select('*').execute();
+    return SafeArea(
+        child: Scaffold(
+            body: FutureBuilder(
+                future: data,
+                builder:
+                    (context, AsyncSnapshot<PostgrestResponse> projectSnap) {
+                  if (!projectSnap.hasData) {
+                    return Container();
+                  }
+                  List<dynamic> shops = projectSnap.data?.data;
+                  return ListView.builder(
+                    itemCount: shops.length,
+                    itemBuilder: (context, index) {
+                      dynamic shop = shops[index];
+                      return Card(
+                          child: ListTile(
+                        leading: Image.network(
+                            'https://tbhtpkmrwtznqzsjlfmo.supabase.co/storage/v1/object/public/shops-content/${shop['logo']}.jpg'),
+                        title: Text(shop['name']),
+                        subtitle: Text(shop['description']),
+                      ));
+                    },
+                  );
+                })));
   }
 }
